1c0105870f8c3c24051f40609692ade7c008993a
Finn <finn-klessascheck@o2mail.de>
2017-11-21 16:15:05 +0100

    Create initial config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+  ethereum: {
+    averageBlockTime:
+      'https://api.nanopool.org/v1/eth/network/avgblocktime',
+    timeToNextEpoch:
+      'https://api.nanopool.org/v1/eth/network/timetonextepoch',
+  },
+}
+
+module.exports = config
