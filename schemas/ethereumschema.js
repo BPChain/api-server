72a5f5a6b22d12d40dd304ebff76ddc13fd21e2a
Simon Siegert <sisiegert@uni-potsdam.de>
2017-11-28 15:35:24 +0100

    Move model to schema

--- schemas/ethereumschema.js
+++ schemas/ethereumschema.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-module.exports = () => {
-  
-  const EthereumStatSchema = new Schema({
+module.exports = () => {  
+  return new Schema({
     id: {type: String},
     hostId: {type: String},
     hashrate: {type: Number},
     avgBlocktime: {type: Number, default: 20},
     uptime: {type: Number},
     gasPrice: {type: Number},
-    avgDiffculty: {type: Number, default: 20},
+    avgDifficulty: {type: Number, default: 20},
   })
-
-  mongoose.model('EthereumSchema', EthereumStatSchema)
 }
