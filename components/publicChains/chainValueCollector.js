547ffe3c244807788816704ea0ee8f4a38fa40db
Tom Lichtenstein <tomli96@web.de>
2018-01-10 13:44:02 +0100

    Ensure the completeness of stored public data

--- components/publicChains/chainValueCollector.js
+++ components/publicChains/chainValueCollector.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const fse = require('fs-extra')
 
+const log = console
 
 module.exports = async (options = {}) => {
   const {chainName} = options
@@ -8,7 +9,7 @@ module.exports = async (options = {}) => {
   const basePath = path.join(__dirname, chainName)
   const files = await fse.readdir(basePath)
 
-  const object = (await Promise.all(files
+  let object = (await Promise.all(files
     .map(async (file) => {
       const filePath = path.join(basePath, file)
       const result = await require(filePath)()
@@ -19,9 +20,29 @@ module.exports = async (options = {}) => {
       return result
     }, {})
 
-  return Object.assign(object, {
+  object = await Object.assign(object, {
     timeStamp: (new Date)
       .toUTCString(),
     chain: chainName,
   })
+
+  try {
+    const hasAllKeys = object.expectedKeys.every((item) => {
+      const keyExists = object.hasOwnProperty(item)
+      if (!keyExists) {
+        log.error('!!! Missing public key:', item)
+      }
+      return keyExists
+    })
+    const expectedKeysDeleted = delete object.expectedKeys
+
+    if (!hasAllKeys || !expectedKeysDeleted) {
+      return null
+    }
+    return object
+  }
+  catch (error) {
+    log.error(error)
+    return null
+  }
 }
---
+++ components/publicChains/ethereum/expectedKeys.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+  return [
+    'chain',
+    'timeStamp',
+    'avgHashrate',
+    'avgBlocktime',
+    'numberOfMiners',
+    'numberOfWorkers',
+    'timeToNextEpoch',
+  ]
+}
