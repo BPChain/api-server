295fa06df3055986a304a50ac0b20d17213c281c
Simon Siegert <sisiegert@uni-potsdam.de>
2018-01-24 14:47:11 +0100

    Refactor logHTMLGenerator, use reduce and dict

--- src/logHTMLGenerator.js
+++ src/logHTMLGenerator.js
@@ -1,6 +1,29 @@
 module.exports = async (options = {}) => {
   const {data} = options
-  let html = `<!DOCTYPE html>
+  const logLevels = {
+    10: '<td style="color:#0066ff;">trace</td>',
+    20: '<td style="color:#009933;">debug</td>',
+    30: '<td style="color:#756647;">info</td>',
+    40: '<td style="color:#ff9900;">warn</td>',
+    50: '<td style="color:#ff0000;">error</td>',
+    60: '<td style="color:#800000;">fatal</td>',
+  }
+
+  const logLines = data.reduce((html, logEntry) => {
+    const logLevelLine =
+      logLevels[logEntry.logLevel] ||
+      '<td style="color:#0066ff;">trace</td>'
+
+    return `${html}
+      <tr>
+        <td>${logEntry.timeStamp}</td>
+        ${logLevelLine}
+        <td>${logEntry.log}</td>
+      </tr>`
+  }, '')
+
+
+  return `<!DOCTYPE html>
     <HTML>
       <HEAD>
         <style>
@@ -11,48 +34,8 @@ module.exports = async (options = {}) => {
       </HEAD>
       <BODY style='font-family:verdana;'>
         <table>
-    `
-  let logLevelLine = '<td style="color:#0066ff";>trace</td>'
-
-  data.forEach(log => {
-    switch (log.logLevel) {
-    case 10: {
-      logLevelLine = '<td style="color:#0066ff";>trace</td>'
-    } break
-    case 20: {
-      logLevelLine = '<td style="color:#009933";>debug</td>'
-    } break
-    case 30: {
-      logLevelLine = '<td style="color:#756647";>info</td>'
-    } break
-    case 40: {
-      logLevelLine = '<td style="color:#ff9900";>warn</td>'
-    } break
-    case 50: {
-      logLevelLine = '<td style="color:#ff0000";>error</td>'
-    } break
-    case 60: {
-      logLevelLine = '<td style="color:#800000";>fatal</td>'
-    } break
-    default: {
-      logLevelLine = '<td style="color:#0066ff";>trace</td>'
-      break
-    }
-    }
-
-    html += `<tr>
-        <td>${log.timeStamp}</td>
-        ${logLevelLine}
-        <td>${log.log}</td>
-      </tr>`
-
-  })
-
-
-  html += `</table>
-    </BODY>
-  </HTML>`
-
-
-  return html
+        ${logLines}
+        </table>
+        </BODY>
+    </HTML>`
 }
