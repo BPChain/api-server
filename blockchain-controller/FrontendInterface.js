f901c7e8a6634eca91bea9acd98b273c613f0797
Tom Lichtenstein <tomli96@web.de>
2018-02-13 15:40:55 +0100

    Add blockchain controller that can handle multiple clients

---
+++ blockchain-controller/FrontendInterface.js
@@ -0,0 +1,28 @@
+const ws = require('ws')
+const WebSocketServer = ws.Server
+
+
+class FrontendInterface {
+  constructor (options = {}) {
+    const {port = 40404, log = console, blockchainController} = options
+    this.port = port
+    this.log = log
+    this.blockchainController = blockchainController
+  }
+
+  start () {
+    const wsServer = new WebSocketServer({port: this.port})
+    this.log.info(`Intern Server waiting for connections on port ${this.port}`)
+    wsServer.on('connection', client => {
+      this.log.info(`Frontend connected on port ${this.port}`)
+      client.on('message', data => {
+        this.log.info(`Redirecting message ${data}`)
+        this.blockchainController.sendMessage(JSON.parse(data))
+      })
+    })
+  }
+
+
+}
+
+module.exports = FrontendInterface
