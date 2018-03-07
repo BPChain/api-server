e45db5c0d29b219396d746434886c6d32bea143b
bisbaldi <annika.baldi@googlemail.com>
2018-03-07 14:06:16 +0100

    Create tests for itemNumberLimiter

--- test/component/dataStorageAccessor/model/testItemNumberLimiter.js
+++ test/component/dataStorageAccessor/model/testItemNumberLimiter.js
@@ -1,1 +1,40 @@
-module.exports = (numberOfItems) => Math.min(10000, Math.max(1, numberOfItems))
+const assert = require('assert')
+
+const describe = require('mocha').describe
+const before = require('mocha').before
+const it = require('mocha').it
+const after = require('mocha').after
+
+const itemNumberLimiter = require(
+  '../../../../components/dataStorageAccessor/model/itemNumberLimiter',
+)
+
+const log = console
+
+describe('ItemNumberLimiter', () => {
+  before(() => {
+    log.info('Start testing ItemNumberLimiter')
+  })
+  it('should execute without throwing an error', () => {
+    assert.doesNotThrow(() => {
+      itemNumberLimiter()
+    })
+    assert(typeof itemNumberLimiter === 'function')
+  })
+  it('should return min for negative values', () => {
+    assert.equal(itemNumberLimiter(-5), 1)
+  })
+  it('should return min for zero value', () => {
+    assert.equal(itemNumberLimiter(0), 1)
+  })
+  it('should return value for value between min and max', () => {
+    assert.equal(itemNumberLimiter(50), 50)
+  })
+  it('should return max for value greater than max', () => {
+    assert.equal(itemNumberLimiter(1000000), 10000)
+  })
+
+  after(() => {
+    log.info('End testing ItemNumberLimiter')
+  })
+})
