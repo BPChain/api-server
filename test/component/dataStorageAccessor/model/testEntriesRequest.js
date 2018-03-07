4a972d313928c0d572c782688ca34022ffced74c
bisbaldi <annika.baldi@googlemail.com>
2018-03-07 13:35:28 +0100

    Create tests for EntriesRequest

---
+++ test/component/dataStorageAccessor/model/testEntriesRequest.js
@@ -0,0 +1,44 @@
+const assert = require('assert')
+
+const describe = require('mocha').describe
+const before = require('mocha').before
+const it = require('mocha').it
+const after = require('mocha').after
+
+const entriesRequest = require(
+  '../../../../components/dataStorageAccessor/model/entriesRequest'
+)
+
+const log = console
+
+describe('EntriesRequest', () => {
+  before(() => {
+    log.info('Start testing EntriesRequest')
+  })
+  describe('EntriesRequest execution', () => {
+    const mockCollection = {
+      find: () => mockCollection,
+      limit: () => mockCollection,
+      sort: () => mockCollection,
+      toArray: () => mockCollection,
+      reverse: () => [100000] }
+    it('should execute without throwing an error', () => {
+      assert.doesNotThrow(() => {
+        entriesRequest({
+          numberOfItems: -2,
+          connection: {
+            db: {collection: () => {
+              return mockCollection
+            }}}})
+          .then(data => {
+            assert.equal(data.length, 1)
+          })
+      })
+      assert(typeof entriesRequest === 'function')
+    })
+  })
+  after(() => {
+    log.info('End testing EntriesRequest')
+  })
+})
+
---
+++ test/component/dataStorageAccessor/model/testItemNumberLimiter.js
@@ -0,0 +1,1 @@
+module.exports = (numberOfItems) => Math.min(10000, Math.max(1, numberOfItems))
