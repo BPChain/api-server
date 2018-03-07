73841542af74d528550154ba82008e5df88b9114
Finn K <finn-klessascheck@o2mail.de>
2018-03-07 16:45:37 +0100

    Add test for password hashing

---
+++ test/component/authenticationHandler/testPasswordHashing.js
@@ -0,0 +1,29 @@
+/* eslint-disable max-len */
+
+const describe = require('mocha').describe
+const before = require('mocha').before
+const it = require('mocha').it
+const after = require('mocha').after
+const assert = require('assert')
+const passwordEncryption = require('../../../components/authenticationHandler/passwordHashing')
+const log = console
+
+
+
+const hashToExpect = '58585acd673067f96bea32a1c57bf3fc3fd5a42678567e72d5cb0ab7f08ea41dcf3a41af96c53948e13184ae6fe6cd0b8b4193fc593dfb2693b00c2b0ee7a316'
+const password = 'abc'
+const salt = '123'
+
+describe('passwordHashing', () => {
+  before(() => {
+    log.info('Start testing password hashing')
+  })
+  describe('#pashwordHashing()', () => {
+    it('should return the expected hash value', async () => {
+      assert.equal(passwordEncryption({password, salt}), hashToExpect)
+    })
+  })
+  after(() => {
+    log.info('End testing user validation')
+  })
+})
