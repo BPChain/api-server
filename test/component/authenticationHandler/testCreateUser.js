a3f04057aa1d9a587160ac607d9492c3c7347ee9
Finn K <finn-klessascheck@o2mail.de>
2018-03-12 10:47:02 +0100

    Add working test for user creation

--- test/component/authenticationHandler/testCreateUser.js
+++ test/component/authenticationHandler/testCreateUser.js
@@ -6,46 +6,30 @@ const it = require('mocha').it
 const after = require('mocha').after
 const assert = require('assert')
 const createUser = require('../../../components/authenticationHandler/createUser')
-const userSchema = require('../../../components/authenticationHandler/userSchema')
 const log = console
 
 
 const password = 'abc'
 const username = 'some'
 
-
-
-/* eslint-disable no-unused-vars  */
+/* eslint-disable no-unused-vars */
 const optionsA = {
   username,
   password,
   connection: {
     model: (string, type) => {
-      if (type === 'Schema') {
-        return {
-          findOne: (object, callbackFunction) => {
-            callbackFunction(false, true)
-          },
-          findOneAndUpdate: (query, user, opts, callbackFunction) => {
-            callbackFunction(false, true)
-
-          },
+      return class User {
+        static findOne (object, callbackFunction) {
+          callbackFunction(false, true)
         }
-      }
-      else {
-        return function User () {
-          function findOne (object, callbackFunction) {
-            callbackFunction(false, true)
-          }
-          function findOneAndUpdate (query, user, opts, callbackFunction) {
-            callbackFunction(false, true)
-          }
+        static findOneAndUpdate (query, user, opts, callbackFunction) {
+          callbackFunction(false, true)
         }
-
+        lines () { }
+        save () { }
       }
     },
   },
-
   log,
 }
 
@@ -54,32 +38,80 @@ const optionsB = {
   password,
   connection: {
     model: (string, type) => {
-      if (type === userSchema) {
-        return {
-          findOne: (object, callbackFunction) => {
-            callbackFunction(false, true)
-          },
-          findOneAndUpdate: (query, user, opts, callbackFunction) => {
-            callbackFunction(false, true)
-          },
+      return class User {
+        static findOne (object, callbackFunction) {
+          callbackFunction(true, false)
         }
-      }
-      else {
-        return function User () {
-          return {
-            lines: () => {},
-            save: () => {},
-          }
+        static findOneAndUpdate (query, user, opts, callbackFunction) {
+          callbackFunction(true, false)
         }
+        lines () { }
+        save () { }
       }
     },
   },
-  log: {
-    info: () => {},
-    debug: () => {},
-  },
+  log,
 }
 
+const optionsC = {
+  username,
+  password,
+  connection: {
+    model: (string, type) => {
+      return class User {
+        static findOne (object, callbackFunction) {
+          callbackFunction(false, false)
+        }
+        static findOneAndUpdate (query, user, opts, callbackFunction) {
+          callbackFunction(true, false)
+        }
+        lines () { }
+        save () { }
+      }
+    },
+  },
+  log,
+}
+
+const optionsD = {
+  username,
+  password,
+  connection: {
+    model: (string, type) => {
+      return class User {
+        static findOne (object, callbackFunction) {
+          callbackFunction(false, false)
+        }
+        static findOneAndUpdate (query, user, opts, callbackFunction) {
+          callbackFunction(false, false)
+        }
+        lines () { }
+        save () { }
+      }
+    },
+  },
+  log,
+}
+
+const optionsE = {
+  username,
+  password,
+  connection: {
+    model: (string, type) => {
+      return class User {
+        static findOne (object, callbackFunction) {
+          callbackFunction(false, false)
+        }
+        static findOneAndUpdate (query, user, opts, callbackFunction) {
+          callbackFunction(false, true)
+        }
+        lines () { }
+        save () { }
+      }
+    },
+  },
+  log,
+}
 
 describe('validateUser', () => {
   before(() => {
@@ -87,7 +119,19 @@ describe('validateUser', () => {
   })
   describe('#validateUser()', () => {
     it('should return false when password does not match', async () => {
-      assert.equal(await createUser(optionsB), true)
+      assert.equal(await createUser(optionsA), false)
+    })
+    it('should return false when username lookup fails', async () => {
+      assert.equal(await createUser(optionsB), false)
+    })
+    it('should return false when user insertion errors', async () => {
+      assert.equal(await createUser(optionsC), false)
+    })
+    it('should return false when user insertion does not yield a result', async () => {
+      assert.equal(await createUser(optionsD), false)
+    })
+    it('should return true when username is free and user insertion succeeds', async () => {
+      assert.equal(await createUser(optionsE), true)
     })
   })
   after(() => {
