f482a7b17a94ac7c50d87e80c5a9d6bee77cbe70
Simon Siegert <sisiegert@uni-potsdam.de>
2018-03-13 12:21:36 +0100

    Create test for aggregator

---
+++ test/component/dataStorageAccessor/model/testAggregator.js
@@ -0,0 +1,76 @@
+const assert = require('assert')
+
+const describe = require('mocha').describe
+const before = require('mocha').before
+const it = require('mocha').it
+const after = require('mocha').after
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+chai.use(chaiAsPromised)
+const expect = require('chai').expect
+
+const aggregator = require(
+  '../../../../components/dataStorageAccessor/model/aggregator',
+)
+
+const log = console
+
+describe('Aggregator', () => {
+  before(() => {
+    log.info('Start testing Aggregator')
+  })
+
+  it('should throw an error when no options are supplied', () => {
+    return expect(aggregator()).to.eventually.be.rejectedWith(TypeError)
+  })
+
+  it('should do a timeSpan request if startTime and endTime are provided', async () => {
+    const mockCollection = {
+      find: () => mockCollection,
+      limit: () => mockCollection,
+      sort: () => mockCollection,
+      toArray: () => mockCollection,
+      filter: () => mockCollection,
+      map: () => [1, 2, 3],
+    }
+    const result = await aggregator({
+      connection: {
+        db: {
+          collection: () => {
+            return mockCollection
+          },
+        },
+      },
+      startTime: '2012-09-27',
+      endTime: '2012-09-27',
+    })
+    assert.deepEqual(result, [1, 2, 3])
+  })
+  it('should do an entries request if numberOfItems is provided', async () => {
+    const mockCollection = {
+      find: () => mockCollection,
+      limit: () => mockCollection,
+      sort: () => mockCollection,
+      toArray: () => mockCollection,
+      filter: () => mockCollection,
+      reverse: () => mockCollection,
+      map: () => [4, 5, 6],
+    }
+    const result = await aggregator({
+      connection: {
+        db: {
+          collection: () => {
+            return mockCollection
+          },
+        },
+      },
+      numberOfItems: 20,
+    })
+    assert.deepEqual(result, [4, 5, 6])
+  })
+  after(() => {
+    log.info('End testing Aggregator')
+  })
+})
