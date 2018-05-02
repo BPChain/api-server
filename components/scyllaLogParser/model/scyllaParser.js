5da00ec8c2f8f4234b2039cb5522faaa6e83b1f3
Simon Siegert <sisiegert@uni-potsdam.de>
2018-05-02 17:08:31 +0200

    Calculate deltas for all execution nodes

--- components/scyllaLogParser/model/scyllaParser.js
+++ components/scyllaLogParser/model/scyllaParser.js
@@ -11,27 +11,54 @@ module.exports = () => {
     nodes: [],
   }
   */
-
   const nodes = {
 
   }
-
+  let minDate = 'Z'
   parseString(input, {trim: true}, (error, result) => {
-    const eventLog = result.log.trace.map(trace => {
-      return trace.event.filter(event => event.string.length === 4)
+    const eventLog = result.log.trace
+      .map(trace => trace.event.filter(event => event.string.length === 4))
+
+    eventLog.forEach(instance => {
+      instance.forEach(event => {
+        const node = event.string.find(key => key.$.key === 'org:resource').$.value
+        const date = event.date[0].$.value
+        minDate = [minDate, date].sort()[0]
+        if (nodes[node]) {
+          nodes[node].push({date, size: 5}) // size = key: /.*\.payload$/
+        }
+        else {
+          nodes[node] = [{date, size: 5}]
+        }
+      })
     })
 
-    const instance = eventLog[0]
+    Object.keys(nodes)
+      .forEach(key => {
+        nodes[key] = nodes[key].sort((a, b) => {
+          if (new Date(a.date) < new Date(b.date)) {
+            return -1
+          }
+          if (new Date(a.date) === new Date(b.date)) {
+            return 0
+          }
+          if (new Date(a.date) > new Date(b.date)) {
+            return 1
+          }
+        })
+      })
 
-    instance.forEach(event => {
-      const node = event.string.find(key => key.$.key === 'org:resource').$.value
-      const date = event.date[0].$.value
-      if (nodes[node]) {
-        nodes[node].push({date, size: 5}) // size = key: /.*\.payload$/
-      }
-      else {
-        nodes[node] = [{date, size: 5}]
-      }
-    })
+    Object.keys(nodes)
+      .map(item => {
+        return nodes[item] = nodes[item].reduce((list, object, index, elements) => {
+          if (index === 0) {
+            object.delta = new Date(object.date) - new Date(minDate)
+          }
+          else {
+            object.delta =  new Date(object.date) - new Date(elements[index - 1].date)
+          }
+          return list.concat(object)
+        }, [])
+      })
   })
 }
