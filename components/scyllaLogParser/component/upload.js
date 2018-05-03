a125164ba295efd670bc25cd354501ffdbff9f98
Simon Siegert <sisiegert@uni-potsdam.de>
2018-05-03 16:21:54 +0200

    Adapt binary source of request data to fixed component path

--- components/scyllaLogParser/component/upload.js
+++ components/scyllaLogParser/component/upload.js
@@ -6,7 +6,7 @@ module.exports = (request, response) => {
   if (!request.files) {
     response.sendStatus(400)
   }
-  const file = request.file.scyllalog
+  const file = request.scyllalog
   const name = crypto
     .createHash('sha512')
     .update(file.name)
